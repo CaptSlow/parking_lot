623fde1ccda4929c4b27cdd6fa9f639efd83bcf5
CaptSlow <daniel.forrester@live.co.uk>
2019-05-11 00:35:45 +0100

    amended and added to ParkingLot and tests - constructor, print, firstFreeSpace

--- src/main/java/com/danf/parkinglot/ParkingLot.java
+++ src/main/java/com/danf/parkinglot/ParkingLot.java
@@ -1,19 +1,65 @@
 package com.danf.parkinglot;
 
-import java.util.List;
-
 public class ParkingLot {
 
-    private long numSpaces;
-    private List<ParkingSpace> spaceList;
+    private int numSpaces;
+    private ParkingSpace[] spaceList;
 
-    public void parkCar(Car newCar){
-        // find earlier free space in list
-        // add newCar to list
-        // add info to log
+    // constructor
+    public ParkingLot(int numSpaces) {
+        this.numSpaces = numSpaces;
+        this.spaceList = new ParkingSpace[numSpaces];
+
+        for (int i=0; i<this.spaceList.length; i++){
+            this.spaceList[i] = new ParkingSpace();
+        }
     }
 
-    public void removeCar(long spaceNumber){
+    public ParkingSpace[] getSpaceList() {
+        return spaceList;
+    }
+
+    //    ******* METHODS ********
+
+    public void print(){
+        System.out.println("No" + "\t" + "Registration Slot No." + "\t" + "Colour");
+        int i = 0;
+        for ( ParkingSpace ps : this.spaceList ) {
+            i++;
+            System.out.println(i + "\t" + ps.toString());
+        }
+    }
+
+    // method to return the lowest number parking space that is free
+    public int firstFreeSpace(){
+        int spotNum = 0;
+
+        while ( !this.spaceList[spotNum].isFree() ){
+            spotNum++;
+        }
+
+        return spotNum==this.spaceList.length ? -1 : spotNum;
+    }
+
+    public void parkInGivenSpot(Car car, int spotNum){
+        this.spaceList[spotNum] = new ParkingSpace(car);
+    }
+
+
+    // method to park car - in lowest number parking space
+    public void parkCar(Car newCar) {
+        // find first free space
+        // add newCar to list spacelist in that index
+        // print out
+    }
+
+    private void addCar(Car newCar, int position){
+
+
+
+    }
+
+    public void removeCar(int spaceNumber){
         // remove car at given place from list
         // add info to log
     }
--- src/test/java/com/danf/parkinglot/ParkingLotTest.java
+++ src/test/java/com/danf/parkinglot/ParkingLotTest.java
@@ -1,16 +1,40 @@
 package com.danf.parkinglot;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ParkingLotTest {
 
-    private final int PARKING_SIZE_I = 6;
-    private final int PARKING_SIZE_II = 2;
-    private ParkingLot PARKING_LOT_I = new ParkingLot(PARKING_SIZE_I);
+    private final int PARKING_SIZE = 6;
+    private String IS_FREE_STR = "is free";
 
+    @Test
+    public void makeParkingLotNumSpacesTest(){
+        ParkingLot myParking = new ParkingLot(PARKING_SIZE);
+
+        Assert.assertNotNull(myParking);
+        Assert.assertEquals(IS_FREE_STR, myParking.getSpaceList()[PARKING_SIZE-1].toString());
+        Assert.assertEquals(PARKING_SIZE, myParking.getSpaceList().length);
+    }
 
     @Test
     public void firstFreeSpaceTest(){
+//        ParkingLot myParking = new ParkingLot(PARKING_SIZE);
+    }
+
+    @Test
+    public void parkInGivenSpotTest(){
+        ParkingLot myParking = new ParkingLot(PARKING_SIZE);
+        int spotNum = 3;
+        Car myCar = new Car("ab-01-cd-1234","BLUE");
+
+        myParking.parkInGivenSpot(myCar, spotNum);
+
+        myParking.print();
+
+        Car actualCar = myParking.getSpaceList()[spotNum].getCarInSpace();
+
+        Assert.assertEquals(myCar,actualCar);
 
     }
 
