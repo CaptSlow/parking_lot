05339468c1301fe1c9d8d60444e1c0da1a29d9a9
CaptSlow <daniel.forrester@live.co.uk>
2019-05-10 20:36:23 +0100

    stubbed out ParkingLotTest methods

---
+++ src/test/java/com/danf/parkinglot/ParkingLotTest.java
@@ -0,0 +1,42 @@
+package com.danf.parkinglot;
+
+import org.junit.Test;
+
+public class ParkingLotTest {
+
+    private final int PARKING_SIZE_I = 6;
+    private final int PARKING_SIZE_II = 2;
+    private ParkingLot PARKING_LOT_I = new ParkingLot(PARKING_SIZE_I);
+
+
+    @Test
+    public void firstFreeSpaceTest(){
+
+    }
+
+    @Test
+    public void createParkingLotTest(){
+
+    }
+
+    @Test
+    public void parkCarTest(){
+
+    }
+
+    @Test
+    public void removeCarTest(){
+
+    }
+
+    @Test
+    public void getWhiteCarsSlotNumbersTest(){
+
+    }
+
+    @Test
+    public void getWhiteCarsRegistrationNumbersTest(){
+
+    }
+
+}
