1f0872ed870f6789b8a6762a95bd7a8efa493b22
CaptSlow <daniel.forrester@live.co.uk>
2019-05-11 21:44:12 +0100

    completed read in file mode

--- src/main/java/com/danf/parkinglot/AppParkingLot.java
+++ src/main/java/com/danf/parkinglot/AppParkingLot.java
@@ -1,5 +1,9 @@
 package com.danf.parkinglot;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -85,6 +89,21 @@ public class AppParkingLot {
     }
 
     public void readInFileMode(String fileName){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] inArr = line.split(" ");
+                this.determineCommand(inArr);
+            }
+
+        } catch (FileNotFoundException fne){
+            System.out.println("FileNotFoundException - cannot find file " + fne.getMessage());
+        } catch (IOException ioe){
+            System.out.println("IOException - Unable to read line in file " + ioe.getMessage());
+        }
+
 
     }
 
@@ -112,13 +131,19 @@ public class AppParkingLot {
             case "create_parking_lot":
                 if (inArr.length==2) {
                     this.newParkingLotInteractive(Integer.parseInt(inArr[1]));
-                    this.interactiveMode();
+                    if (this.runningMode==0){
+                        this.interactiveMode();
+                    }
                 } else if (inArr.length==1) {
                     System.out.println("The Number of parking spaces is required");
-                    this.interactiveMode();
+                    if (this.runningMode==0){
+                        this.interactiveMode();
+                    }
                 } else {
                     System.out.println("Too many arguments");
-                    this.interactiveMode();
+                    if (this.runningMode==0){
+                        this.interactiveMode();
+                    }
                 }
                 break;
 
@@ -128,34 +153,46 @@ public class AppParkingLot {
                     String inColour=inArr[2];
                     Car inCar = new Car(inReg,inColour);
                     this.parkingLot.parkCar(inCar);
-                    this.interactiveMode();
+                    if (this.runningMode==0){
+                        this.interactiveMode();
+                    }
                 }
                 break;
 
             case "leave":
                 int inSlot = Integer.parseInt(inArr[1]);
                 this.parkingLot.removeCar(inSlot-1);
-                this.interactiveMode();
+                if (this.runningMode==0){
+                    this.interactiveMode();
+                }
                 break;
 
             case "status":
                 this.parkingLot.print();
-                this.interactiveMode();
+                if (this.runningMode==0){
+                    this.interactiveMode();
+                }
                 break;
 
             case "registration_numbers_for_cars_with_colour":
                 this.parkingLot.registrationNumbersForCarsColour(inArr[1]);
-                this.interactiveMode();
+                if (this.runningMode==0){
+                    this.interactiveMode();
+                }
                 break;
 
             case "slot_numbers_for_cars_with_colour":
                 System.out.print(this.parkingLot.slotNumbersForCarsColour(inArr[1]));
-                this.interactiveMode();
+                if (this.runningMode==0){
+                    this.interactiveMode();
+                }
                 break;
 
             case "slot_number_for_registration_number":
                 this.parkingLot.slotForRegNumber(inArr[1]);
-                this.interactiveMode();
+                if (this.runningMode==0){
+                    this.interactiveMode();
+                }
                 break;
 
             case "exit":
@@ -164,7 +201,9 @@ public class AppParkingLot {
 
             default:
                 System.out.println("Unknown command. Type exit to exit");
-                this.interactiveMode();
+                if (this.runningMode==0){
+                    this.interactiveMode();
+                }
         }
 
     }
