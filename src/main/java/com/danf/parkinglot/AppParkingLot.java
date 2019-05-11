2e20928717c70145598fbec5ec77691311d12bb5
CaptSlow <daniel.forrester@live.co.uk>
2019-05-11 20:34:29 +0100

    added get user input and determine command to AppParkingLot

--- src/main/java/com/danf/parkinglot/AppParkingLot.java
+++ src/main/java/com/danf/parkinglot/AppParkingLot.java
@@ -1,9 +1,13 @@
 package com.danf.parkinglot;
 
+import java.util.Scanner;
+
 /**
  * Class for the parkingLotApp
  */
 public class AppParkingLot {
+
+    ParkingLot parkingLot;
 
     /**
      * runningMode determines how the app runs
@@ -17,6 +21,10 @@ public class AppParkingLot {
         System.out.println("Starting parking lot application");
     }
 
+    /**
+     * Getter and setter
+     * @return running mode
+     */
     public int getRunningMode() {
         return runningMode;
     }
@@ -28,7 +36,6 @@ public class AppParkingLot {
     public void run(String[] args) {
 
         this.checkVariables(args);
-
 
         switch (this.getRunningMode()) {
             case -1:
@@ -82,7 +89,85 @@ public class AppParkingLot {
     }
 
     public void interactiveMode(){
+        String[] inArr = this.getUserInput();
 
+        if (inArr.length>3){
+            System.out.println("Too many arguments");
+            this.interactiveMode();
+        } else {
+            this.determineCommand(inArr);
+        }
+
+    }
+
+    public String[] getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().split(" ");
+    }
+
+    public void determineCommand(String[] inArr){
+        String command = inArr[0];
+
+        switch (command) {
+            case "create_parking_lot":
+                if (inArr.length==2) {
+                    this.newParkingLotInteractive(Integer.parseInt(inArr[1]));
+                    this.interactiveMode();
+                } else if (inArr.length==1) {
+                    System.out.println("The Number of parking spaces is required");
+                    this.interactiveMode();
+                } else {
+                    System.out.println("Too many arguments");
+                    this.interactiveMode();
+                }
+                break;
+
+            case "park":
+                if (inArr.length==3) {
+                    String inReg=inArr[1];
+                    String inColour=inArr[2];
+                    Car inCar = new Car(inReg,inColour);
+                    this.parkingLot.parkCar(inCar);
+                    this.interactiveMode();
+                }
+                break;
+
+            case "leave":
+                int inSlot = Integer.parseInt(inArr[1]);
+                this.parkingLot.removeCar(inSlot-1);
+                this.interactiveMode();
+                break;
+
+            case "status":
+                this.parkingLot.print();
+                this.interactiveMode();
+                break;
+
+            case "registration_numbers_for_cars_with_colour":
+                this.interactiveMode();
+                break;
+
+            case "slot_numbers_for_cars_with_colour ":
+                this.interactiveMode();
+                break;
+
+            case "slot_number_for_registration_number":
+                this.interactiveMode();
+                break;
+
+            case "Exit":
+                System.out.println("Exiting");
+                break;
+
+            default:
+                System.out.println("Unknown command. Type Exit to exit");
+                this.interactiveMode();
+        }
+
+    }
+
+    public void newParkingLotInteractive(int numSpaces){
+        this.parkingLot = new ParkingLot(numSpaces);
     }
 
 }
--- src/test/java/com/danf/parkinglot/AppParkingLotTest.java
+++ src/test/java/com/danf/parkinglot/AppParkingLotTest.java
@@ -3,6 +3,9 @@ package com.danf.parkinglot;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class AppParkingLotTest {
 
     @Test
@@ -33,6 +36,34 @@ public class AppParkingLotTest {
         myApp.setRunningMode(-2);
         myApp.checkVariables(iiiArgs);
         Assert.assertEquals(-1, myApp.getRunningMode());
+
+    }
+
+//    @Test
+//    public void splitInteractiveInputTest(){
+//        AppParkingLot myApp = new AppParkingLot();
+//
+//        String in = "create_parking_lot 6";
+//        String[] inArr = myApp.splitInteractiveInput(in);
+//
+//        Assert.assertEquals("create_parking_lot", inArr[0] );
+//        Assert.assertEquals("6", inArr[1] );
+//
+//    }
+
+    @Test
+    public void determineCommandTest(){
+//        System.out.println("determineCommandTest");
+//        AppParkingLot myApp = new AppParkingLot();
+//        String[] in = {"create_parking_lot", "6"};
+//        String expPrint = "Created a parking lot with 6 slots\r\n";
+//
+//        ByteArrayOutputStream actualPrint = new ByteArrayOutputStream();
+//        System.setOut(new PrintStream(actualPrint));
+//
+//        myApp.determineCommand(in);
+//
+//        Assert.assertEquals(expPrint, actualPrint.toString());
 
     }
 
